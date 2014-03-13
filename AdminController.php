f6ff88d2300a462ede9927b602236a8d8d7aa90a
Algimantas <algimantas@impresspages.org>
2014-03-13 11:16:16 +0200

    Cleanup

--- AdminController.php
+++ AdminController.php
@@ -19,34 +19,37 @@ class AdminController extends \Ip\GridController
 
 
         return array(
-            'title' => __('Person list', 'ipAdmin', FALSE),
-            'table' => 'person',
-//            'sortField' => 'gridOrder',
-//            'createPosition' => 'top',
+            'title' => __('Person list (gridExample table)', 'ipAdmin', FALSE),
+            'table' => 'gridExample',
+
+            // you can comment sortField and createPosition out
+            'sortField' => 'gridOrder',
+            'createPosition' => 'top', // 'top' or 'bottom'
+
 //            'pageSize' => 3,
             'fields' => array(
                 array(
-                    'label' => __('Name', 'GridTest', FALSE),
+                    'label' => __('Name', 'GridExample', FALSE),
                     'field' => 'name',
                 ),
                 array(
                     'type' => 'Select',
-                    'label' => __('Age', 'GridTest', FALSE),
+                    'label' => __('Age', 'GridExample', FALSE),
                     'field' => 'age',
                     'values' => array(
-                        array('young', __('Young', 'GridTest', FALSE)),
-                        array('old', __('Old', 'GridTest', FALSE))
+                        array('young', __('Young', 'GridExample', FALSE)),
+                        array('old', __('Old', 'GridExample', FALSE))
                     )
                 ),
                 array(
                     'type' => 'Checkbox',
-                    'label' => __('In love', 'GridTest', FALSE),
+                    'label' => __('In love', 'GridExample', FALSE),
                     'showInList' => true,
                     'field' => 'inLove'
                 ),
                 array(
                     'type' => 'RepositoryFile',
-                    'label' => __('CV', 'GridTest', FALSE),
+                    'label' => __('CV', 'GridExample', FALSE),
                     'showInList' => true,
                     'field' => 'cv'
                 )
--- Setup/Worker.php
+++ Setup/Worker.php
@@ -1,16 +1,16 @@
 <?php
 
-namespace Plugin\GridTest\Setup;
+namespace Plugin\GridExample\Setup;
 
 class Worker extends \Ip\SetupWorker{
 
     public function activate()
     {
-        $table = ipTable('person');
+        $table = ipTable('gridExample');
         ipDb()->execute("
 CREATE TABLE IF NOT EXISTS $table (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `gridOrder` int(11) NOT NULL,
+  `gridOrder` DOUBLE NOT NULL,
   `inLove` int(1) NOT NULL,
   `age` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS $table (
 
     public function deactivate()
     {
-        $table = ipTable('person');
+        $table = ipTable('gridExample');
         try {
             ipDb()->execute("
               DROP TABLE $table;
